b6a4ed4d7d38f7b2e3a7ede3f6d332629e7ff2b9
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-11-22 16:33:47 +0530

    did small changes to the homepage and add animation

--- src/components/directory-menu/directory-menu.component.jsx
+++ src/components/directory-menu/directory-menu.component.jsx
@@ -12,32 +12,38 @@ class DirectoryMenu extends Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1533055640609-24b498dfd74c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+
           id: 1,
           linkUrl: 'shop/hats',
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1591851395349-6d8c2fe76e24?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
           id: 2,
           linkUrl: 'shop/jackets',
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1579338559194-a162d19bf842?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
           id: 3,
           linkUrl: 'shop/sneakers',
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens',
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1550246140-29f40b909e5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens',
@@ -49,8 +55,8 @@ class DirectoryMenu extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, title, imageUrl,size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        {this.state.sections.map(({ id, title, imageUrl, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
