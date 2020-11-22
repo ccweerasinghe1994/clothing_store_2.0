cffc508d54854c183f8464493224cead4c158ff5
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-11-22 15:42:18 +0530

    add two new components and one page

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import DirecoryMenu from '../../components/directory-menu/directory-menu.component';
+
+const Homepage = () => (
+  <div className='homepage'>
+    <DirecoryMenu />
+  </div>
+);
+
+export default Homepage;
